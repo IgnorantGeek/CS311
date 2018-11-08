cef8936c5a96a62491745a7d8522ca6061bd792f
Nick Heisler <nheisler@iastate.edu>
2018-11-08 16:53:56 -0600

    created skeleton code for all required methods

--- nheisler-PA2/src/ImageProcessor.java
+++ nheisler-PA2/src/ImageProcessor.java
@@ -1,6 +1,46 @@
-import java.lang.String;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 
 public class ImageProcessor
 {
-  //class stuff
+  String filepath;
+  int height;
+  int width;
+
+  public ImageProcessor(String fname)
+  {
+    this.filepath = fname;
+    try 
+    {
+      File file = new File(this.filepath);
+      Scanner scan = new Scanner(file);
+      this.height = scan.nextInt();
+      this.width = scan.nextInt();
+      while (scan.hasNextLine())
+      {
+        //read the info for pixel stuff
+      }
+      scan.close();
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("Could not open the specified file");
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    //main method stuff
+  }
+
+  ArrayList<ArrayList<Integer>> getImportance()
+  {
+    return null;
+  }
+
+  void writeReduced(int k, String fname)
+  {
+    //do the thing and write to file
+  }
 }
