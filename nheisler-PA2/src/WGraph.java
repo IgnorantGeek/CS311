a98e582a89b4049a4601cec7db651e567dbae674
Nick Heisler <nheisler@iastate.edu>
2018-11-13 09:41:04 -0600

    Fixed NullPointerException when adding nodes to ArrayList

--- nheisler-PA2/src/WGraph.java
+++ nheisler-PA2/src/WGraph.java
@@ -23,7 +23,7 @@ public class WGraph
    */
   private class Node
   {
-    ArrayList<Node> edges;
+    ArrayList<Node> connections = new ArrayList<Node>(); //duh
     int position[] = new int[2];
     private boolean isVertex()
     {
@@ -39,46 +39,7 @@ public class WGraph
   int numVert = 0;
   int numEdges = 0;
   String pathtofile;
-  
-  /**
-   * Helper method to print the graph, features a try-catch block that attempts to open a scanner on the input 
-   * file from the constructor. Doesn't print if the input file is invalid. 
-   */
-  private void printGraph()
-  {
-    try
-    {
-      File file = new File(this.pathtofile);
-      Scanner scan = new Scanner(file);
-      scan.close();
-    }
-    catch (FileNotFoundException e)
-    {
-      //don't do anything, the constructor handles error printing, this only makes sure 
-      //we are not printing when there is no file.
-      return;
-    }
-    System.out.println("Total number of vertices in graph: " + this.numVert);
-    System.out.println("Max number of vertices: " + this.vertices.length);
-    System.out.println("Total number of edges in graph: " + this.numEdges);
-    System.out.println("Max number of edges: " + this.edges.length);
-    for (int i = 0; i < numEdges; i++) 
-    {
-      Node from = edges[i].start;
-      Node to = edges[i].end;
-      int cost = edges[i].weight;
-      System.out.print(from.position[0]); 
-      System.out.print(' '); System.out.print(from.position[1]);
-      System.out.print(' ');
-      System.out.print(to.position[0]);
-      System.out.print(' ');
-      System.out.print(to.position[1]);
-      System.out.print(' ');
-      System.out.print(cost);
-      System.out.println();
-    }
-  }
-  
+
   /**
    * Constructor for a new WGraph with some read file, the semantic of the read file is as follows:
    * (1) First line contains a number indicating the number of vertices in the graph
@@ -108,6 +69,7 @@ public class WGraph
         boolean newEdge = false;
         start.position[0] = scan.nextInt();
         start.position[1] = scan.nextInt();
+        start.connections.add(end);
         end.position[0] = scan.nextInt();
         end.position[1] = scan.nextInt();
         e.weight = scan.nextInt();
@@ -176,7 +138,6 @@ public class WGraph
     return null;
   }
 
-<<<<<<< HEAD
   /**
    * Helper method to print the graph
    */
@@ -204,8 +165,6 @@ public class WGraph
     }
   }
 
-=======
->>>>>>> ebe5760c15e52fbb439275230b839278bc8b92f3
   public static void main(String[] args)
   {
     //here is where main stuff will go
